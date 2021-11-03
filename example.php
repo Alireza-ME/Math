54c8342ec2aeb5b81247b4c2a1ebacd4c088bf1b
Alireza <74488703+Alireza-ME@users.noreply.github.com>
2021-11-03 17:00:44 +0330

    Add files via upload
    Example Use Class

---
+++ example.php
@@ -0,0 +1,13 @@
+<?php
+
+//Use class
+include_once('Math.class.php');
+
+/*@method of receiving prime numbers*/
+Math::primenumber(1,10);
+/*@method Factorial calculation of a number*/
+print Math::Factorial(6);
+
+
+
+
